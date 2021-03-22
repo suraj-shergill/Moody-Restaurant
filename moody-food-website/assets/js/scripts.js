dcc95470e60c03c05461511deef1b109cf70d912
suraj-shergill <58194084+suraj-shergill@users.noreply.github.com>
2021-03-21 23:52:03 -0400

    Add files via upload

---
+++ moody-food-website/assets/js/scripts.js
@@ -0,0 +1,12 @@
+var currentWidth = window.innerWidth;
+var calculatedWidth = currentWidth - 1366;
+
+wind
+if(calculatedWidth > 0){
+	Math.abs(calculatedWidth);
+	document.documentElement.style.fontSize = "" + (16px - 1px*Math.ceil(calculatedWidth/70));
+
+
+} else{
+	Math.abs(calculatedWidth);
+}
---
+++ moody-food-website/index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;600;800;900&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;500;700;800;900&display=swap" rel="stylesheet">
+		<script src="https://kit.fontawesome.com/74d71b825a.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="assets/styles/reset-css.css"/>
+		<link rel="stylesheet" type="text/css" href="assets/styles/styles.css"/>
+		
+		<title>Moody Food</title>
+	</head>
+	<body>
+		<section id="main">
+			<nav>
+				<div id="nav-logo" class="rectangle">#<span>pizzeria.</span></div>
+				<ul>
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Contacts</a></li>
+				</ul>
+			</nav>
+			<div id="main-text-container" class="rectangle">
+				<p>
+					<span>GRAND</span><br> OPENING
+				</p>
+				<a href="#">
+					Read More
+				</a>
+				<p class="invisible">
+					<span>GRAND</span><br> OPENING
+				</p>			
+			</div>
+		</section>
+		
+	
+		<section id="whats-new-section">
+		
+			<div id="whats-new-top">
+				<img src="assets/images/pizza-whats-new.png"/>
+				<div class="rectangle"><span>A</span></div>
+				<div id="whats-next-text">
+					<p>new format</p>
+					<p>What new?</p>
+					<p>|  Sed do eiusmod tempor incididunt ut Labore et</p>
+					<p>|  Dolore magna aliqua </p>
+					<p>|  Quis ipsum suspendisse ultrices gravida </p>
+				</div>
+			</div>
+			
+			<div id="whats-new-bottom">
+				<div id="bottom-text-container">
+					<div class="text-section">
+						<p>| new format</p>
+						<p>Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+					</div>
+					<div class="text-section">
+						<p>|| master classes</p>
+						<p>Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+					</div>
+					<div class="text-section">
+						<p>||| pizza restaurant</p>
+						<p>Lorem ipsum dolor sit amet, consecte sectetur adipisicing elit, tation omne ullamco laboris nisi ut aliqolore.</p>
+					</div>
+					
+				</div>
+			</div>
+		</section>
+		
+		<section id="menu-section">
+			<div class="opacity">
+				<p class="title">#<span>pizzeria.</span></p>
+				<p class="subtitle">OUR NEW MENU</p>
+				
+				<div id="menu-section-main-container">
+					<div id="menu-section-text-container">
+						<div class="big-rectangle"><span>A</span></div>
+						<div class="menu-section-text">
+							<p>|  EIUSMOD SED DO</p>
+							<p>(tempor, incididunt, utas, labore, etws)</p>
+						</div>
+						<div class="menu-section-text">
+							<p>||  DO DOLORE</p>
+							<p>(magna, aliqua, lorem)</p>
+						</div>
+						<div class="menu-section-text">
+							<p>|||  EIUSMOD SED DO</p>
+							<p>(tempor, incididunt, utas, labore, etws)</p>
+						</div>
+						<div class="menu-section-text">
+							<p>||  DO DOLORE</p>
+							<p>(magna, aliqua, lorem)</p>
+						</div>
+					</div>
+					<div id="menu-section-prices">
+						<p>10$</p>
+						<p>14$</p>
+						<p>20$</p>
+						<p>10$</p>
+					</div>
+				</div>
+				<div class="thin-rectagle"><span>A</span></div>
+				<div id="menu-section-bottom-text">
+					<p>||| _ top in menu</p>
+					<p>|| _ sale iteam</p>
+					<p>| _ made by shef</p>
+				</div>
+			</div>
+		</section>
+		
+		<section id="about-section">
+			<div>
+				<p class="about-title">ABOUT OUR <span>CONCEPT</span></p>
+				<p class="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+				<div id="about-section-bottom-text">
+					<p>Pizza</p>
+					<p>Modern concept</p>
+					<p>Story from shef</p>
+					<p>Gallery</p>
+				</div>
+			</div>
+		</section>
+		
+		<section id="masterclass-section">
+			<div id="masterclass-container">
+				<p class="masterclass-title">MASTERCLASS</p>
+				<div id="masterclass-rectangle"><span>A</span></div>
+				<div id="masterclass-section-text">
+					<p>| THE RECIPE FOR</p>
+					<p>THE MOST</p>
+					<p>DELICIOUS PIZZA</p>
+					<p>DOUGH |</p>
+					<p>waiting for you!</p>
+					<div class="masterclass-section-text-bottom">
+						<p>Price_<span>15$</span></p>
+						<p>12:00-15:00</p>
+					</div>
+					<a href="#"><p class="apply-button">APPLY</p></a>
+				</div>
+			</div>
+		</section>
+		
+		<section id="gallery">
+			<p id="gallery-title">GALLERY</p>
+			<img id="gallery-rectangle" src="assets/images/gallery-rectangle.png"/>
+			<img id="gallery-pizza" src="assets/images/gallery-pizza.png"/>
+			<p id="gallery-text">#pizzeria.</p>
+			<p id="gallery-bottom-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis </p>
+			<div id="socialmedia-section">
+				<div id="socialmedia-icons">
+					<p>| find us here</p>
+					<p>
+						<a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+						<a href="#"><i class="fab fa-facebook-square fa-2x"></i></a>
+						<a href="#"><i class="fab fa-twitter-square fa-2x"></i></a>
+						<a href="#"><i class="fab fa-whatsapp-square fa-2x"></i></a>
+					</p>
+				</div>
+				<div id="socialmedia-website">
+					<p>| ask as here</p>
+					<a href="#"><p>www.#pizzeria.com</p></a>
+				</div>
+			</div>
+			<div id="name">
+				<p>Name Name</p>
+			</div>
+		</section>
+		
+		<section id="footer">
+			<div id="footer-about">
+				<p>ABOUT</p>
+				<p>Lorem ipsum dolor sit amet, consec <br>tetur adipisicing elit, sed do eiusmod tempor <br>incididunt ultimam quantum</p>
+			</div>
+			<div id="footer-list">
+				<div>
+					<p>MENU<span>AAa</span></p>
+					<ul>
+						<li>Team</li>
+						<li>Join Us</li>
+						<li>Ethic</li>
+						<li>Goals</li>
+					</ul>
+				</div>
+				<div>
+					<p>SHEF<span>AAAa</span></p>
+					<ul>
+						<li>Team</li>
+						<li>Join Us</li>
+						<li>Ethic</li>
+						<li>Goals</li>
+					</ul>
+				</div>
+				<div>
+					<p>CONTACT<span>a</span></p>
+					<ul>
+						<li>Team</li>
+						<li>Join Us</li>
+						<li>Ethic</li>
+						<li>Goals</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+		
+		<script src="assets/js/scripts.js"></script>
+	</body>
+</html>
